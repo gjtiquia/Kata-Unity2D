eed84a7b173df7c62d0f029b36e5826c1b02242c
GJ Tiquia <gershom.tiquia@gmail.com>
2023-07-19 08:57:34 +0800

    feat: counter can increment and decrement

--- Assets/_Project/__Tests__/EditMode/CounterTest.cs
+++ Assets/_Project/__Tests__/EditMode/CounterTest.cs
@@ -9,11 +9,31 @@ namespace Kata.Tests
     public class CounterTest
     {
         [Test]
-        public void CounterStartsWithZero()
+        public void StartsWithZero()
         {
             Counter counter = new Counter();
             int count = counter.GetCount();
             Assert.That(count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void CanIncrement()
+        {
+            Counter counter = new Counter();
+            counter.Increment();
+
+            int count = counter.GetCount();
+            Assert.That(count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void CanDecrement()
+        {
+            Counter counter = new Counter();
+            counter.Decrement();
+
+            int count = counter.GetCount();
+            Assert.That(count, Is.EqualTo(-1));
         }
     }
 
@@ -29,6 +49,16 @@ namespace Kata.Tests
         public int GetCount()
         {
             return _count;
+        }
+
+        public void Increment()
+        {
+            _count++;
+        }
+
+        public void Decrement()
+        {
+            _count--;
         }
     }
 }
