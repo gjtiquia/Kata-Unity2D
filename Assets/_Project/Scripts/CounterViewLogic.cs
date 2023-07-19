a4204de1f26b7c28547e539cc731c11586ae3233
GJ Tiquia <gershom.tiquia@gmail.com>
2023-07-19 09:16:46 +0800

    feat: added Counter to CounterViewLogic

--- Assets/_Project/Scripts/CounterViewLogic.cs
+++ Assets/_Project/Scripts/CounterViewLogic.cs
@@ -3,28 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Kata.Core;
 
-
-public class CounterViewLogic : MonoBehaviour
+namespace Kata
 {
-    [SerializeField] private TextMeshProUGUI _countText;
-    [SerializeField] private Button _incrementButton;
-    [SerializeField] private Button _decrementButton;
-
-    private void Awake()
+    public class CounterViewLogic : MonoBehaviour
     {
-        _countText.text = "0";
-        _incrementButton.onClick.AddListener(IncrementOnClick);
-        _decrementButton.onClick.AddListener(DecrementOnClick);
-    }
+        [SerializeField] private TextMeshProUGUI _countText;
+        [SerializeField] private Button _incrementButton;
+        [SerializeField] private Button _decrementButton;
 
-    private void IncrementOnClick()
-    {
-        Debug.Log("increment!");
-    }
+        private Counter _counter;
 
-    private void DecrementOnClick()
-    {
-        Debug.Log("decrement!");
+        private void Awake()
+        {
+            _counter = new Counter();
+            _countText.text = _counter.GetCount().ToString();
+
+            _incrementButton.onClick.AddListener(IncrementOnClick);
+            _decrementButton.onClick.AddListener(DecrementOnClick);
+        }
+
+        private void IncrementOnClick()
+        {
+            Debug.Log("increment!");
+            _counter.Increment();
+        }
+
+        private void DecrementOnClick()
+        {
+            Debug.Log("decrement!");
+            _counter.Decrement();
+        }
     }
 }
+
